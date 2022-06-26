5917d2cca4f60f498de4afc2047a128bef9ca7be
Jefferson Carlos <jeff.carlosbd@gmail.com>
2022-06-26 17:04:11 -0300

    feat: cria caso de uso para mapear e indexar um objeto num contexto e indice definidos

---
+++ src/domain/usecases/MapAndIndexAnObject/UseCase.spec.ts
@@ -0,0 +1,42 @@
+import { MapTheObject } from '../../entities/MapTheObject'
+import { IndexerService } from '../../entities/_services'
+import { MapAndIndexAnObject } from './UseCase'
+
+class IndexerStub implements IndexerService {
+  async index(): Promise<void> {
+    return undefined
+  }
+}
+
+function makeSut() {
+  const mapper = new MapTheObject()
+  const indexerStub = new IndexerStub()
+  const sut = new MapAndIndexAnObject(mapper, indexerStub)
+  return { sut, mapper, indexerStub }
+}
+const anyObject = { id: 1, name: 'any', any: 'any' }
+const fakeSettings = {
+  context: 'any',
+  index: 'any',
+  properties: ['id', 'name']
+}
+
+describe(MapAndIndexAnObject.name, () => {
+  test('deve mapear o objeto com as propriedades definidas', async () => {
+    const { sut, mapper } = makeSut()
+    const mapSpy = jest.spyOn(mapper, 'map')
+    await sut.index(anyObject, fakeSettings)
+    expect(mapSpy).toHaveBeenCalledWith(anyObject, fakeSettings.properties)
+  })
+
+  test('deve indexar os dados mapeados no contexto e indice definidos', async () => {
+    const { sut, mapper, indexerStub } = makeSut()
+    jest.spyOn(mapper, 'map').mockReturnValue({ id: 1 })
+    const indexSpy = jest.spyOn(indexerStub, 'index')
+    await sut.index(anyObject, fakeSettings)
+    expect(indexSpy).toHaveBeenCalledWith(
+      `${fakeSettings.context}-${fakeSettings.index}`,
+      { id: 1 }
+    )
+  })
+})
---
+++ src/domain/usecases/MapAndIndexAnObject/UseCase.ts
@@ -0,0 +1,20 @@
+import { MapTheObject } from '../../entities/MapTheObject'
+import { IndexerService, UnknownObject } from '../../entities/_services'
+
+export interface IndexationSettings {
+  index: string
+  context: string
+  properties: string[]
+}
+
+export class MapAndIndexAnObject {
+  constructor(private mapper: MapTheObject, private indexer: IndexerService) {}
+
+  async index(object: UnknownObject, settings: IndexationSettings) {
+    const mappedObject = this.mapper.map(object, settings.properties)
+    await this.indexer.index(
+      `${settings.context}-${settings.index}`,
+      mappedObject
+    )
+  }
+}
