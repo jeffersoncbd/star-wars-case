50b6c6f3d23233134749714222da3570cf4749f7
Jefferson Carlos <jeff.carlosbd@gmail.com>
2022-06-27 14:25:52 +0000

    chore: ignora arquivos que não fazem sentido serem testados

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,13 @@ export default {
   clearMocks: true,
 
   collectCoverage: false,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/index.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/application.ts',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/_domain/entities/_protocols/*',
+    '!<rootDir>/src/_domain/entities/_services/*'
+  ],
   coverageDirectory: '__tests__/coverage',
   coverageProvider: 'v8',
 
