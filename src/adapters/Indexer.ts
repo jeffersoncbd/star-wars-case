2d83d4d2c61f49601f692372974e65998661a65b
Jefferson Carlos <jeff.carlosbd@gmail.com>
2022-06-28 01:14:35 +0000

    fix: extrai ID para indexar documento

--- src/adapters/Indexer.ts
+++ src/adapters/Indexer.ts
@@ -3,7 +3,8 @@ import { UnknownObject } from '../_domain/entities/_protocols'
 import { IndexerService } from '../_domain/entities/_services'
 
 export class IndexerAdapter implements IndexerService {
-  async index(index: string, body: UnknownObject): Promise<void> {
-    await openSearchClient.index({ index, body })
+  async index(index: string, object: UnknownObject): Promise<void> {
+    const { id, ...body } = object
+    await openSearchClient.index({ id: id as string, index, body })
   }
 }
