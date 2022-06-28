1da4797751ccf7a7d2a176eedcb1f6fc6c57bcf2
Jefferson Carlos <jeff.carlosbd@gmail.com>
2022-06-28 05:29:15 +0000

    refactor: remove código denecessário já que outro endpoint faz mesma operação

--- src/_domain/entities/_services/Indexer.ts
+++ src/_domain/entities/_services/Indexer.ts
@@ -3,11 +3,3 @@ import { UnknownObject } from '../_protocols'
 export interface IndexerService {
   index(data: UnknownObject, index: string, type?: string): Promise<void>
 }
-
-export interface IndexExistenceCheckService {
-  check(index: string): Promise<boolean>
-}
-
-export interface IndexCreationService {
-  create(index: string): Promise<void>
-}
--- src/services/Express/routes.ts
+++ src/services/Express/routes.ts
@@ -1,26 +1,29 @@
 import express from 'express'
 import { openSearchClient } from '../OpenSearch'
-import { adaptAnRequestHandlerFromControllerToCreateAnIndex } from './handlers/createAnIndex'
 import { adaptAnRequestHandlerFromControllerToIndexAnEndpoint } from './handlers/indexAnEndpoint'
 
 const router = express.Router()
 
-router.get('/indices/:name/:query', async (request, response) => {
-  const { name, query } = request.params
-  const openSearchResponse = await openSearchClient.search({
-    index: name,
-    body: { query: { match: { name: { query } } } }
+router.get('/find/:index/:query', async (request, response) => {
+  const { index, query } = request.params
+  const body = { query: { match: { name: { query } } } }
+  const openSearchResponse = await openSearchClient.search({ index, body })
+  const resultList = openSearchResponse.body.hits.hits.map((document: any) => {
+    const urlParts = document._id.split('/')
+    return {
+      id: urlParts[urlParts.length - 2],
+      name: document._source.name,
+      type: document._index
+    }
   })
-
-  return response.json(openSearchResponse.body)
+  return response.json(resultList)
 })
 
 router.get('/indices', async (_, response) => {
   const openSearchResponse = await openSearchClient.cat.indices()
   return response.json(openSearchResponse.body.split('\n'))
 })
-router.post('/indices', adaptAnRequestHandlerFromControllerToCreateAnIndex())
-router.patch(
+router.post(
   '/indices/:name',
   adaptAnRequestHandlerFromControllerToIndexAnEndpoint()
 )
